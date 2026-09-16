75a5fc9a10b3b0c108e98a0d862a521558476a8e
Mehedi MehediSumu Sumu <mehedihasanshopnil.jr@gmail.com>
2026-09-16 19:02:20 +0600

    new update on the gmail adding

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,6 +2,7 @@ const defaultAuthorizedUserEmails = [
   "ashorangelake@gmail.com",
   "ocampojenis333@gmail.com",
   "brian.caceres001@gmail.com",
+  "skmehedihasan.jr1@gmail.com",
 ];
 
 const authorizedUserEmails = (
